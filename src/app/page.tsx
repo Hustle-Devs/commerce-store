61e00ad6792f1f6e658cbc7af25c159d7fb07d89
Saim <smsaimhossain@gmail.com>
2025-04-16 23:03:00 +0600

    Create: Testimonial section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import LatestTemplates from "@/components/Home/LatestTemplates";
 import StaffPicks from "@/components/Home/StaffPicks";
-import Testimonials from "@/components/Testimonial/Testimonials";
+import Testimonial from "@/components/Testimonial/Testimonial";
 
 const page = () => {
   return (
     <>
       <LatestTemplates /> 
       <StaffPicks />
-      <Testimonials />
+      <Testimonial />
     </>
   );
 };
--- src/components/Testimonial/Testimonial.tsx
+++ src/components/Testimonial/Testimonial.tsx
@@ -4,10 +4,14 @@ const Testimonial = () => {
   return (
     <div className="container">
       <div className="testimonial-section">
-        <h3 className="testimonial-type">Testimonial</h3>
+        <h5 className="testimonial-type text-[14px] leading-[22px]">
+          Testimonial
+        </h5>
       </div>
       <div className="testimonial-heading">
-        <h2 className="testimonial-heading-details">What designers think</h2>
+        <h2 className="testimonial-heading-details leading-[54px] font-semibold">
+          What designers think
+        </h2>
       </div>
       <div className="testimonial-users">
         <div className="testimonial-user-details">
@@ -22,7 +26,7 @@ const Testimonial = () => {
                 />
               </div>
               <div className="user-details">
-                <h3 className="user-details-heading-three">Omar D</h3>
+                <h5 className="user-details-heading-three">Omar D</h5>
                 <div className="icons">
                   <Image
                     src="/icons/star.png"
@@ -58,8 +62,10 @@ const Testimonial = () => {
               </div>
             </div>
             <div className="user-about">
-              <h2 className="user-name">Nayzak UI Kit</h2>
-              <p className="user-paragraph">
+              <h5 className="text-[14px] leading-[22px] pt-[16px] font-inter">
+                Nayzak UI Kit
+              </h5>
+              <p className="text-[16px] leading-[26px] font-normal font-inter text-muted-foreground pt-[14px]">
                 Clonify is a game-changer. Instead of drowning in an endless
                 chain of UI kits, there is clear and easy accountability meaning
                 tasks actually get done!
@@ -77,7 +83,7 @@ const Testimonial = () => {
                 />
               </div>
               <div className="user-details">
-                <h3 className="user-details-heading-three">Omar D</h3>
+                <h5 className="user-details-heading-three">Omar D</h5>
                 <div className="icons">
                   <Image
                     src="/icons/star.png"
@@ -113,8 +119,10 @@ const Testimonial = () => {
               </div>
             </div>
             <div className="user-about">
-              <h2 className="user-name">Nayzak UI Kit</h2>
-              <p className="user-paragraph">
+              <h5 className="text-[14px] leading-[22px] pt-[16px] font-inter">
+                Nayzak UI Kit
+              </h5>
+              <p className="text-[16px] leading-[26px] font-normal font-inter text-muted-foreground pt-[14px]">
                 Clonify has helped my team and I stay on the same page.
                 Previously, we were all over the board. Using Clonify has
                 definitely saved us time and money.
@@ -132,7 +140,7 @@ const Testimonial = () => {
                 />
               </div>
               <div className="user-details">
-                <h3 className="user-details-heading-three">Omar D</h3>
+                <h5 className="user-details-heading-three">Omar D</h5>
                 <div className="icons">
                   <Image
                     src="/icons/star.png"
@@ -168,8 +176,10 @@ const Testimonial = () => {
               </div>
             </div>
             <div className="user-about">
-              <h2 className="user-name">Nayzak UI Kit</h2>
-              <p className="user-paragraph">
+              <h5 className="text-[14px] leading-[22px] pt-[16px] font-inter">
+                Nayzak UI Kit
+              </h5>
+              <p className="text-[16px] leading-[26px] font-normal font-inter text-muted-foreground pt-[14px]">
                 If you are a business owner, and you don't have Clonify in your
                 toolkit just yet, I highly recommend that you check it out.
               </p>
@@ -190,7 +200,7 @@ const Testimonial = () => {
                 />
               </div>
               <div className="user-details">
-                <h3 className="user-details-heading-three">Omar D</h3>
+                <h5 className="user-details-heading-three">Omar D</h5>
                 <div className="icons">
                   <Image
                     src="/icons/star.png"
@@ -226,8 +236,10 @@ const Testimonial = () => {
               </div>
             </div>
             <div className="user-about">
-              <h2 className="user-name">Nayzak UI Kit</h2>
-              <p className="user-paragraph">
+              <h5 className="text-[14px] leading-[22px] pt-[16px] font-inter">
+                Nayzak UI Kit
+              </h5>
+              <p className="text-[16px] leading-[26px] font-normal font-inter text-muted-foreground pt-[14px]">
                 If you want to take your business to the next level, use Clonify
                 and don't look any further.
               </p>
@@ -244,7 +256,7 @@ const Testimonial = () => {
                 />
               </div>
               <div className="user-details">
-                <h3 className="user-details-heading-three">Omar D</h3>
+                <h5 className="user-details-heading-three">Omar D</h5>
                 <div className="icons">
                   <Image
                     src="/icons/star.png"
@@ -280,8 +292,10 @@ const Testimonial = () => {
               </div>
             </div>
             <div className="user-about">
-              <h2 className="user-name">Nayzak UI Kit</h2>
-              <p className="user-paragraph">
+              <h5 className="text-[14px] leading-[22px] pt-[16px] font-inter">
+                Nayzak UI Kit
+              </h5>
+              <p className=" font-[400] text-[14px] text-muted-foreground lg:text-[16px] leading-[26px]">
                 I have been using Clonify for over a year now and I love it! I
                 can't imagine life without it. It's so easy to use, and the
                 customer service is more than great.
@@ -299,7 +313,7 @@ const Testimonial = () => {
                 />
               </div>
               <div className="user-details">
-                <h3 className="user-details-heading-three">Omar D</h3>
+                <h5 className="user-details-heading-three">Omar D</h5>
                 <div className="icons">
                   <Image
                     src="/icons/star.png"
@@ -335,8 +349,10 @@ const Testimonial = () => {
               </div>
             </div>
             <div className="user-about">
-              <h2 className="user-name">Nayzak UI Kit</h2>
-              <p className="user-paragraph">
+              <h5 className="text-[14px] leading-[22px] pt-[16px] font-inter">
+                Nayzak UI Kit
+              </h5>
+              <p className="text-[16px] leading-[26px] font-normal font-inter text-muted-foreground pt-[14px]">
                 Everyone’s on the same page. Many of our people are not very
                 organized naturally, so Clonify is a godsend!
               </p>
@@ -357,7 +373,7 @@ const Testimonial = () => {
                 />
               </div>
               <div className="user-details">
-                <h3 className="user-details-heading-three">Omar D</h3>
+                <h5 className="user-details-heading-three">Omar D</h5>
                 <div className="icons">
                   <Image
                     src="/icons/star.png"
@@ -393,8 +409,10 @@ const Testimonial = () => {
               </div>
             </div>
             <div className="user-about">
-              <h2 className="user-name">Nayzak UI Kit</h2>
-              <p className="user-paragraph">
+              <h5 className="text-[14px] leading-[22px] pt-[16px] font-inter">
+                Nayzak UI Kit
+              </h5>
+              <p className="text-[16px] leading-[26px] font-normal font-inter text-muted-foreground pt-[14px]">
                 We can't understand how we've been living without Clonify.
               </p>
             </div>
@@ -410,7 +428,7 @@ const Testimonial = () => {
                 />
               </div>
               <div className="user-details">
-                <h3 className="user-details-heading-three">Omar D</h3>
+                <h5 className="user-details-heading-three">Omar D</h5>
                 <div className="icons">
                   <Image
                     src="/icons/star.png"
@@ -446,8 +464,10 @@ const Testimonial = () => {
               </div>
             </div>
             <div className="user-about">
-              <h2 className="user-name">Nayzak UI Kit</h2>
-              <p className="user-paragraph">
+              <h5 className="text-[14px] leading-[22px] pt-[16px] font-inter">
+                Nayzak UI Kit
+              </h5>
+              <p className="text-[16px] leading-[26px] font-normal font-inter text-muted-foreground pt-[14px]">
                 With Clonify, communication between all of us is far more
                 efficient. A game changer.
               </p>
@@ -464,7 +484,7 @@ const Testimonial = () => {
                 />
               </div>
               <div className="user-details">
-                <h3 className="user-details-heading-three">Omar D</h3>
+                <h5 className="user-details-heading-three">Omar D</h5>
                 <div className="icons">
                   <Image
                     src="/icons/star.png"
@@ -500,8 +520,10 @@ const Testimonial = () => {
               </div>
             </div>
             <div className="user-about">
-              <h2 className="user-name">Nayzak UI Kit</h2>
-              <p className="user-paragraph">
+              <h5 className="text-[14px] leading-[22px] pt-[16px] font-inter">
+                Nayzak UI Kit
+              </h5>
+              <p className="text-[16px] leading-[26px] font-normal font-inter text-muted-foreground pt-[14px]">
                 I would recommend Clonify for anyone trying to get the word out
                 about their business. It has saved me so much time!
               </p>
