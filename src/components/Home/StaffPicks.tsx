a7fe5876c30004d869dd3140d7f4730810c8190c
Md Rasel <mdrasel551219@gmail.com>
2025-04-21 22:14:16 +0600

    Update: Amination On Staff Picks Section

--- src/components/Home/StaffPicks.tsx
+++ src/components/Home/StaffPicks.tsx
@@ -1,4 +1,5 @@
-import React, { lazy } from "react";
+import FadeUp from "@/components/animations/FadeUp";
+import ZoomIn from "@/components/animations/ZoomIn";
 
 const staffPicks = [
   {
@@ -59,21 +60,29 @@ export default function StaffPicks() {
           {staffPicks.map((template, id) => (
             <div key={id} className="p-4">
               <div className="">
-                <img
-                  src={template.image}
-                  alt={template.title}
-                  className="w-full h-full object-cover"
-                />
+                <ZoomIn>
+                  <img
+                    src={template.image}
+                    alt={template.title}
+                    className="w-full h-full object-cover"
+                  />
+                </ZoomIn>
               </div>
               <div className="flex justify-between items-center pt-3">
                 <div>
-                  <h4>{template.title}</h4>
-                  <p className="text-muted-foreground">{template.category}</p>
+                  <FadeUp>
+                    <h4>{template.title}</h4>
+                  </FadeUp>
+                  <FadeUp delay={0.4}>
+                    <p className="text-muted-foreground">{template.category}</p>
+                  </FadeUp>
                 </div>
                 <div>
-                  <h5 className="bg-[#F9FAFB] px-3 py-1 rounded-3xl text-secondary-foreground">
-                    {template.price}
-                  </h5>
+                  <FadeUp delay={0.4}>
+                    <h5 className="bg-[#F9FAFB] px-3 py-1 rounded-3xl text-secondary-foreground">
+                      {template.price}
+                    </h5>
+                  </FadeUp>
                 </div>
               </div>
             </div>
