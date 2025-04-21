5db2bab82a8b8b6f2d839ec38748ca5982cbe08d
aimanshafi <shafiaiman88@gmail.com>
2025-04-21 13:21:20 +0600

    Update: Contact Hero Section Component

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "@/components/contact/hero/Hero";
+
+const page = () => {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+};
+
+export default page;
