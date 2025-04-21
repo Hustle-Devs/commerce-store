83e9e13091a473a7f53650da1bed8b815b0a34bd
Md Rasel <mdrasel551219@gmail.com>
2025-04-21 22:42:08 +0600

    Update: Animation on Pro Access Section

--- src/components/Home/pro-cta/ProCta.tsx
+++ src/components/Home/pro-cta/ProCta.tsx
@@ -1,5 +1,6 @@
-import Image from 'next/image';
-import React from 'react'
+import FadeUp from "@/components/animations/FadeUp";
+import ZoomIn from "@/components/animations/ZoomIn";
+import Image from "next/image";
 
 const ProCta = () => {
   return (
@@ -7,106 +8,124 @@ const ProCta = () => {
       <div className="pro-cta">
         <div className="pro-cta-content">
           <div>
-            <h2 className="pro-heading">Pro Access</h2>
-            <p className="pro-paragraph">
-              Launching your next website has never been so fast and effortless.
-              Browse hundreds of beautifully designed Framer templates and go
-              live in minutes.
-            </p>
-            <div className=" flex flex-col sm:flex-row items-center py-[5px] sm:py-[36px]">
+            <FadeUp>
+              <h2 className="pro-heading">Pro Access</h2>
+            </FadeUp>
+            <FadeUp delay={0.3}>
+              <p className="pro-paragraph">
+                Launching your next website has never been so fast and
+                effortless. Browse hundreds of beautifully designed Framer
+                templates and go live in minutes.
+              </p>
+            </FadeUp>
+            <div className="flex sm:flex-row flex-col items-center py-[5px] sm:py-[36px]">
               <div>
-                <div className="icon-area">
-                  <div className="icon-image">
-                    <Image
-                      className=""
-                      src="/icons/badge-icon.png"
-                      width={16}
-                      height={16}
-                      alt="badge-icon"
-                    />
+                <FadeUp delay={0.6}>
+                  <div className="icon-area">
+                    <div className="icon-image">
+                      <Image
+                        className=""
+                        src="/icons/badge-icon.png"
+                        width={16}
+                        height={16}
+                        alt="badge-icon"
+                      />
+                    </div>
+                    <div className="icon-content">
+                      <p className="icon-pra-content">3K+ templates</p>
+                    </div>
                   </div>
-                  <div className="icon-content">
-                    <p className="icon-pra-content">3K+ templates</p>
+                </FadeUp>
+                <FadeUp delay={0.6}>
+                  <div className="icon-area">
+                    <div className="icon-image">
+                      <Image
+                        className=""
+                        src="/icons/badge-icon.png"
+                        width={16}
+                        height={16}
+                        alt="badge-icon"
+                      />
+                    </div>
+                    <div className="icon-content">
+                      <p className="icon-pra-content">Responsive layouts</p>
+                    </div>
                   </div>
-                </div>
-                <div className="icon-area">
-                  <div className="icon-image">
-                    <Image
-                      className=""
-                      src="/icons/badge-icon.png"
-                      width={16}
-                      height={16}
-                      alt="badge-icon"
-                    />
+                </FadeUp>
+                <FadeUp delay={0.6}>
+                  <div className="icon-area">
+                    <div className="icon-image">
+                      <Image
+                        className=""
+                        src="/icons/badge-icon.png"
+                        width={16}
+                        height={16}
+                        alt="badge-icon"
+                      />
+                    </div>
+                    <div className="icon-content">
+                      <p className="icon-pra-content">Google Fonts</p>
+                    </div>
                   </div>
-                  <div className="icon-content">
-                    <p className="icon-pra-content">Responsive layouts</p>
-                  </div>
-                </div>
-                <div className="icon-area">
-                  <div className="icon-image">
-                    <Image
-                      className=""
-                      src="/icons/badge-icon.png"
-                      width={16}
-                      height={16}
-                      alt="badge-icon"
-                    />
-                  </div>
-                  <div className="icon-content">
-                    <p className="icon-pra-content">Google Fonts</p>
-                  </div>
-                </div>
+                </FadeUp>
               </div>
               <div>
-                <div className="icon-area">
-                  <div className="icon-image">
-                    <Image
-                      className=""
-                      src="/icons/badge-icon.png"
-                      width={16}
-                      height={16}
-                      alt="badge-icon"
-                    />
+                <FadeUp delay={0.6}>
+                  <div className="icon-area">
+                    <div className="icon-image">
+                      <Image
+                        className=""
+                        src="/icons/badge-icon.png"
+                        width={16}
+                        height={16}
+                        alt="badge-icon"
+                      />
+                    </div>
+                    <div className="icon-content">
+                      <p className="icon-pra-content">Shared styles</p>
+                    </div>
                   </div>
-                  <div className="icon-content">
-                    <p className="icon-pra-content">Shared styles</p>
+                </FadeUp>
+                <FadeUp delay={0.6}>
+                  <div className="icon-area">
+                    <div className="icon-image">
+                      <Image
+                        className=""
+                        src="/icons/badge-icon.png"
+                        width={16}
+                        height={16}
+                        alt="badge-icon"
+                      />
+                    </div>
+                    <div className="icon-content">
+                      <p className="icon-pra-content">5k Components</p>
+                    </div>
                   </div>
-                </div>
-                <div className="icon-area">
-                  <div className="icon-image">
-                    <Image
-                      className=""
-                      src="/icons/badge-icon.png"
-                      width={16}
-                      height={16}
-                      alt="badge-icon"
-                    />
+                </FadeUp>
+                <FadeUp delay={0.6}>
+                  <div className="icon-area">
+                    <div className="icon-image">
+                      <Image
+                        className=""
+                        src="/icons/badge-icon.png"
+                        width={16}
+                        height={16}
+                        alt="badge-icon"
+                      />
+                    </div>
+                    <div className="icon-content">
+                      <p className="icon-pra-content"> Premium support</p>
+                    </div>
                   </div>
-                  <div className="icon-content">
-                    <p className="icon-pra-content">5k Components</p>
-                  </div>
-                </div>
-                <div className="icon-area">
-                  <div className="icon-image">
-                    <Image
-                      className=""
-                      src="/icons/badge-icon.png"
-                      width={16}
-                      height={16}
-                      alt="badge-icon"
-                    />
-                  </div>
-                  <div className="icon-content">
-                    <p className="icon-pra-content"> Premium support</p>
-                  </div>
-                </div>
+                </FadeUp>
               </div>
             </div>
-            <div className="hidden sm:flex w-[70%] sm:w-[400px] md:w-[500px] lg:w-[540px] bg-[#7E36F4] h-[54px] mx-auto rounded-[40px] items-center justify-center mb-[80px]">
-              <button className="text-[18px] font-semibold leading-[30px] text-[#ffffff]">
-                Start your 7 day trial
-              </button>
+            <div className="hidden sm:flex justify-center items-center bg-[#7E36F4] mx-auto mb-[80px] rounded-[40px] w-[70%] sm:w-[400px] md:w-[500px] lg:w-[540px] h-[54px]">
+              <ZoomIn delay={0.3}>
+                <button className="font-semibold text-[#ffffff] text-[18px] leading-[30px]">
+                  Start your 7 day trial
+                </button>
+              </ZoomIn>
             </div>
           </div>
         </div>
@@ -122,6 +141,6 @@ const ProCta = () => {
       </div>
     </div>
   );
-}
+};
 
-export default ProCta
+export default ProCta;
