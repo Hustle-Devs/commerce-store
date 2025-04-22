3966a0c58b182ecb0f854ffd40826872a49b78cf
Md Rasel <mdrasel551219@gmail.com>
2025-04-22 13:06:32 +0600

    Update: Animation on QuestionSection for Contact Page

--- src/components/contact/questions/Questions.tsx
+++ src/components/contact/questions/Questions.tsx
@@ -1,46 +1,67 @@
+import FadeUp from "@/components/animations/FadeUp";
+import ZoomIn from "@/components/animations/ZoomIn";
+
 const Questions = () => {
   return (
-    <div className="container w-[600px] mx-auto mt-[48px] mb-[80px]">
+    <div className="mx-auto mt-[48px] mb-[80px] w-[600px] container">
       <div className="questions-sec">
-        <h2 className="font-medium text-[28px] md:text-[36px] md:leading-[30px] lg:text-[40px] lg:leading-[48px] font-inter  text-center text-[#000000] dark:text-white">
-          Still got questions?
-        </h2>
-        <p className="text-[#000000]/56 dark:text-white pt-[16px]">
-          Send us a message below and we'll get back to you in 1 business day.
-        </p>
-        <div className="mt-[48px] flex flex-col gap-[24px]">
+        <ZoomIn>
+          <h2 className="font-inter font-medium text-[#000000] text-[28px] md:text-[36px] lg:text-[40px] dark:text-white text-center md:leading-[30px] lg:leading-[48px]">
+            Still got questions?
+          </h2>
+        </ZoomIn>
+        <FadeUp delay={0.2}>
+          <p className="pt-[16px] text-[#000000]/56 dark:text-white">
+            Send us a message below and we'll get back to you in 1 business day.
+          </p>
+        </FadeUp>
+        <div className="flex flex-col gap-[24px] mt-[48px]">
           <div>
-            <label>Name</label>
-            <input
-              type="text"
-              placeholder="Joe Gomez"
-              className="bg-[#F9F6FE] w-full py-[11px] pl-[12px] mt-[10px] text-[14px] font-medium text-[#000000]/56 rounded-[8px]"
-            />
+            <ZoomIn delay={0.2}>
+              <label>Name</label>
+            </ZoomIn>
+            <FadeUp delay={0.2}>
+              <input
+                type="text"
+                placeholder="Joe Gomez"
+                className="bg-[#F9F6FE] mt-[10px] py-[11px] pl-[12px] rounded-[8px] w-full font-medium text-[#000000]/56 text-[14px]"
+              />
+            </FadeUp>
           </div>
           <div>
-            <label>Email</label>
-            <input
-              type="email"
-              placeholder="joe@gomez.com"
-              className="bg-[#F9F6FE] w-full py-[11px] pl-[12px] mt-[10px] text-[14px] font-medium text-[#000000]/56 rounded-[8px]"
-            />
+            <ZoomIn delay={0.2}>
+              <label>Email</label>
+            </ZoomIn>
+            <FadeUp delay={0.2}>
+              <input
+                type="email"
+                placeholder="joe@gomez.com"
+                className="bg-[#F9F6FE] mt-[10px] py-[11px] pl-[12px] rounded-[8px] w-full font-medium text-[#000000]/56 text-[14px]"
+              />{" "}
+            </FadeUp>
           </div>
           <div>
-            <label>Message</label>
-            <textarea
-              className="bg-[#F9F6FE] w-full h-[100px] mt-[10px] text-[14px] font-medium text-[#000000]/56 rounded-[8px] pt-[12px] pl-[12px]"
-              placeholder="Hey, I need help with..."
-            ></textarea>
+            <ZoomIn delay={0.2}>
+              <label>Message</label>
+            </ZoomIn>
+            <FadeUp delay={0.2}>
+              <textarea
+                className="bg-[#F9F6FE] mt-[10px] pt-[12px] pl-[12px] rounded-[8px] w-full h-[100px] font-medium text-[#000000]/56 text-[14px]"
+                placeholder="Hey, I need help with..."
+              ></textarea>
+            </FadeUp>
           </div>
-          <div className=" w-full bg-[#7E36F4] h-[40px] mx-auto rounded-[10px] items-center justify-center mb-[80px] flex">
-            <button className="text-[14px] font-semibold leading-[21px] text-[#ffffff]">
-              Submit
-            </button>
+          <div className="flex justify-center items-center bg-[#7E36F4] mx-auto mb-[80px] rounded-[10px] w-full h-[40px]">
+            <ZoomIn delay={0.2}>
+              <button className="font-semibold text-[#ffffff] text-[14px] leading-[21px]">
+                Submit
+              </button>
+            </ZoomIn>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Questions
+export default Questions;
