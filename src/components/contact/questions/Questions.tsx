4ea0c860489a2262155fd07c3d36e3ba3f354b01
aimanshafi <shafiaiman88@gmail.com>
2025-04-26 22:54:34 +0600

    Update: Contact page button layout

--- src/components/contact/questions/Questions.tsx
+++ src/components/contact/questions/Questions.tsx
@@ -1,56 +1,52 @@
-import FadeUp from "@/components/animations/FadeUp";
-
 const Questions = () => {
   return (
-    <div className="container w-[600px] mx-auto mt-[48px] mb-[80px]">
+    <div className="mx-auto mt-[48px] mb-[80px] w-[600px] container">
       <div className="questions-sec">
-        <h2 className="font-medium text-[28px] md:text-[36px] md:leading-[30px] lg:text-[40px] lg:leading-[48px] font-inter  text-center text-[#000000] dark:text-white">
+        <h2 className="font-inter font-medium text-[#000000] text-[28px] md:text-[36px] lg:text-[40px] dark:text-white text-center md:leading-[30px] lg:leading-[48px]">
           Still got questions?
         </h2>
-        <p className="text-[#000000]/56 dark:text-white pt-[16px] font-normal">
+        <p className="pt-[16px] font-normal text-[#000000]/56 dark:text-white">
           Send us a message below and we'll get back to you in 1 business day.
         </p>
-        <div className="mt-[48px] flex flex-col gap-[24px]">
+        <div className="flex flex-col gap-[24px] mt-[48px]">
           <div>
-            <label className="font-medium text-[14px] font-inter leading-[21px] text-[#000000] dark:text-white">
+            <label className="font-inter font-medium text-[#000000] text-[14px] dark:text-white leading-[21px]">
               Name
             </label>
             <input
               type="text"
               placeholder="Joe Gomez"
-              className="bg-[#F9F6FE] w-full py-[11px] pl-[12px] mt-[10px] text-[14px] font-medium text-[#000000]/56 rounded-[8px]"
+              className="bg-[#F9F6FE] mt-[10px] py-[11px] pl-[12px] rounded-[8px] w-full font-medium text-[#000000]/56 text-[14px]"
             />
           </div>
           <div>
-            <label className="font-medium text-[14px] font-inter leading-[21px] text-[#000000] dark:text-white">
+            <label className="font-inter font-medium text-[#000000] text-[14px] dark:text-white leading-[21px]">
               Email
             </label>
             <input
               type="email"
               placeholder="joe@gomez.com"
-              className="bg-[#F9F6FE] w-full py-[11px] pl-[12px] mt-[10px] text-[14px] font-medium text-[#000000]/56 rounded-[8px]"
+              className="bg-[#F9F6FE] mt-[10px] py-[11px] pl-[12px] rounded-[8px] w-full font-medium text-[#000000]/56 text-[14px]"
             />
           </div>
           <div>
-            <label className="font-medium text-[14px] font-inter leading-[21px] text-[#000000] dark:text-white">
+            <label className="font-inter font-medium text-[#000000] text-[14px] dark:text-white leading-[21px]">
               Message
             </label>
             <textarea
-              className="bg-[#F9F6FE] w-full h-[100px] mt-[10px] text-[14px] font-medium text-[#000000]/56 rounded-[8px] pt-[12px] pl-[12px]"
+              className="bg-[#F9F6FE] mt-[10px] pt-[12px] pl-[12px] rounded-[8px] w-full h-[100px] font-medium text-[#000000]/56 text-[14px]"
               placeholder="Hey, I need help with..."
             ></textarea>
           </div>
-          <div className=" w-full bg-[#7E36F4] h-[40px] mx-auto rounded-[10px] items-center justify-center mb-[80px] flex ">
-            <FadeUp delay={0.3}>
-              <button className="text-[14px] font-semibold leading-[21px] text-[#ffffff]">
-                Submit
-              </button>
-            </FadeUp>
+          <div className="flex justify-center items-center mx-auto mb-[80px]] w-full h-[40px]">
+            <button className="bg-[#7E36F4] rounded-[10px] size-full font-semibold text-[#ffffff] text-[14px] leading-[21px] cursor-pointer">
+              Submit
+            </button>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Questions
+export default Questions;
